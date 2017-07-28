137c13e3665d11796cb69dc10cfccdcaac303b72
Álvaro Fraga <fragaarnaizalvaro@gmail.com>
2017-07-28 21:40:32 +0200

    add home component

---
+++ front/src/app/home-component/home-component.component.ts
@@ -0,0 +1,39 @@
+import { OnInit, OnDestroy, Component } from '@angular/core';
+import { Route } from '../../model/route.model';
+import { Stops } from '../../model/stop.model';
+import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
+import { BusService } from '../../services/busService';
+@Component({
+  selector: 'app-bus',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  title: String;
+  id: String;
+  routes: Observable<Route[]>;
+  stops_detail: Observable<Stops[]>;
+  subscription: Subscription;
+  constructor(private service: BusService) {
+    this.title = 'Horario autobuses';
+  }
+
+  ngOnDestroy() {
+    this.routes = null;
+    this.stops_detail = null;
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  ngOnInit() {
+    this.routes = this.service.getRoutes();
+  }
+
+  OnSelect(id: String) {
+    this.stops_detail = this.service.getDetails(id);
+  }
+
+
+}
