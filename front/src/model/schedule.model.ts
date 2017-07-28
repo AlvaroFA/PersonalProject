585342a1941c7d3373bc64259c494b6b5f783c88
Álvaro Fraga <fragaarnaizalvaro@gmail.com>
2017-07-28 20:03:47 +0200

    Add schedule model v1

--- front/src/model/schedule.model.ts
+++ front/src/model/schedule.model.ts
@@ -1,5 +1,11 @@
-export interface Schedule {
-  weekly_days:string;
-  departures:string;
-}
+export class Schedule {
+  labor_days: String[];
+  saturday_labor_days: String[];
+  holidays_sunday_days: String[];
 
+  constructor(l: String[], s: String[], h: String[]) {
+    this.labor_days = l;
+    this.saturday_labor_days = s;
+    this.holidays_sunday_days = h;
+  }
+}
